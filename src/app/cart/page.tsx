27eab053462df4008ff4cc6d5d4ddd33312fe20d
Sulaimon Omotosho <susudef2u@gmail.com>
2025-11-05 10:07:43 +0100

    Finished Products page, Started Cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const CartPage = () => {
+  return <div>CartPage</div>
+}
+
+export default CartPage
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import SearchBar from './SearchBar'
 import { Bell, Home, ShoppingCart } from 'lucide-react'
+import ShoppingCartIcon from './ShoppingCartIcon'
 
 const Navbar = () => {
   return (
@@ -27,7 +28,7 @@ const Navbar = () => {
           <Home className='w-4 h-4 text-gray' />
         </Link>
         <Bell className='w-4 h-4 text-gray' />
-        <ShoppingCart className='w-4 h-4 text-gray' />
+        <ShoppingCartIcon />
         <Link href='/'>Sign In</Link>
       </div>
     </div>
